a2e77c0e72ac506fe1826b1db0ebc1deacc67e93
Edgar Sanou <esedsa75@gmail.com>
2021-05-20 23:24:33 +0200

    cambios

--- src/SQLCore/SQLCore.java
+++ src/SQLCore/SQLCore.java
@@ -7,7 +7,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SQLCore {
+<<<<<<< Updated upstream
 	private static String urlDatos = "jdbc:mysql://localhost/proyecto?serverTimezone=UTC";
+=======
+	private static String urlDatos = "jdbc:mysql://2.138.10.124/proyecto?serverTimezone=UTC";
+>>>>>>> Stashed changes
 	private static String usuario = "root";
 	private static String pass = "Persiana1234";
 	public static Connection Connection() {
